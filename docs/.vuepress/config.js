cf6e99651e8d6bdd6dca21ec1208aab96cfebbc8
bergiusj <jocke.bergius@live.se>
2018-11-02 11:41:21 +0100

    building for netlify

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,4 @@
 module.exports = {
-	themeConfig: {
-		sidebar: [
-		'/docs/images/',
-		['/', 'HEM']
-		]
-	},
 		base: '/',
 		title: 'BL Design System',
 		description: 'First build with VuePress'
